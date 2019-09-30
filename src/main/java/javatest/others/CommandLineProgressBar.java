4a13fc7107ee57d6e89ba80972fe722b56a37e84
TTG\darekar <sawan.darekar@tomtom.com>
2019-09-30 22:22:24 +0530

    javadocs added

---
+++ src/main/java/javatest/others/CommandLineProgressBar.java
@@ -0,0 +1,13 @@
+package javatest.others;
+
+public class CommandLineProgressBar {
+    public static void main(String[] arg) throws Exception {
+        String anim= "|/-\\";
+        for (int x =0 ; x < 100 ; x++) {
+            String data = "\r" + anim.charAt(x % anim.length()) + " " + x;
+            System.out.write(data.getBytes());
+            Thread.sleep(100);
+        }
+
+    }
+}
--- src/main/java/ocjp/assigments/NullObjectAssignment.java
+++ src/main/java/ocjp/assigments/NullObjectAssignment.java
@@ -5,22 +5,29 @@ import javatest.testBeans.Person;
 public class NullObjectAssignment {
 
     Person nullPerson = null;
+    Person person = new Person("test","city", 23);
 
     public static void main(String[] args) {
         NullObjectAssignment assignment = new NullObjectAssignment();
-        System.out.println("nullString before assignment:" + assignment.nullPerson);
+        System.out.println("nullPerson before assignment:" + assignment.nullPerson);
         assignment.assignObject(assignment.nullPerson);
-        System.out.println("nullString after assignment:" + assignment.nullPerson);
+        System.out.println("nullPerson after assignment:" + assignment.nullPerson);
         assignment.nullPerson = assignment.assignAndReturnObject(assignment.nullPerson);
-        System.out.println("nullString after assignment and return:" + assignment.nullPerson);
+        System.out.println("nullPerson after assignment and return:" + assignment.nullPerson);
+        System.out.println("-----------------------------");
+        System.out.println("person before assignment:" + assignment.person);
+        assignment.assignObject(assignment.person);
+        System.out.println("person after assignment:" + assignment.person);
+        assignment.person = assignment.assignAndReturnObject(assignment.person);
+        System.out.println("person after assignment and return:" + assignment.person);
     }
 
     private void assignObject(Person nullPerson) {
-        nullPerson = new Person("name", "city", 20);
+        nullPerson = new Person("ass_name", "ass_city", 20);
     }
 
     private Person assignAndReturnObject(Person nullPerson) {
-        nullPerson = new Person("name", "city", 20);
+        nullPerson = new Person("ass_ret_name", "ass_ret_city", 20);
         return nullPerson;
     }
 }
